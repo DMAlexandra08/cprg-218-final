55747956bf8fcd2bf625e9272e81ec9b9c82a5f5
DMAlexandra08 <Alexandra.Armstrong@edu.sait.ca>
2025-04-17 17:18:19 -0600

    index and js
    outline for homepage & js for weather

---
+++ main.js
@@ -0,0 +1,9 @@
+fetch('https://api.openweathermap.org/data/2.5/weather?q=Cancun&appid=cfb3d1b143131b3e9f17add9eb31017a&units=metric')
+
+.then(response => response.json())
+.then(data => {
+    console.log(data);
+    console.log(data.main.temp);
+    weather.textContext 
+        = 'Temperature: ' + data.main.temp + '\u00B0C';
+})
