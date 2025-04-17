6e73a15a1ddf02a53b34ff01cbd68d55c96560df
DMAlexandra08 <Alexandra.Armstrong@edu.sait.ca>
2025-04-17 17:28:07 -0600

    Update main.js

--- main.js
+++ main.js
@@ -4,5 +4,5 @@ fetch('https://api.openweathermap.org/data/2.5/weather?q=Cancun&appid=cfb3d1b143
 .then(data => {
     console.log(data);
     console.log(data.main.temp);
-    weather.textContext = "Current Temperature: " + data.main.temp+"\u00B0C";
+    weather.textContext = "Current Temperature: " + data.main.temp+" \u00B0C";
 });
