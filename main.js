30f22287e6b7c5669904004584340aa13987363b
DMAlexandra08 <Alexandra.Armstrong@edu.sait.ca>
2025-04-18 16:01:15 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+const weather = document.getElementById('weather')
+const weathericon = document.getElementById('describe')
+
 fetch('https://api.openweathermap.org/data/2.5/weather?q=Cancun&appid=cfb3d1b143131b3e9f17add9eb31017a&units=metric')
 
 .then(response => response.json())
@@ -7,8 +10,6 @@ fetch('https://api.openweathermap.org/data/2.5/weather?q=Cancun&appid=cfb3d1b143
     weather.textContent = "Current Temperature : " + data.main.temp+" \u00B0C";
 });
 
-const weather = document.getElementById('weather')
-const weathericon = document.getElementById('describe')
 const iconCode = data.weather[0].icon;
 const iconUrl = 'https://openweathermap.org/img/wn/${iconCode}@2x.png';
 weathericon.src = iconUrl;
