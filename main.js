c0c728f83a606f10c3ba10878dd928599f27aaec
DMAlexandra08 <Alexandra.Armstrong@edu.sait.ca>
2025-04-18 15:53:13 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
 const weather = document.getElementById('weather')
 const weathericon = document.getElementById('describe')
+const iconCode = data.weather[0].icon;
+const iconUrl = 'https://openweathermap.org/img/wn/${iconCode}@2x.png'
+weathericon.src = iconUrl;
+console.log(iconUrl);
+
 
 fetch('https://api.openweathermap.org/data/2.5/weather?q=Cancun&appid=cfb3d1b143131b3e9f17add9eb31017a&units=metric')
 
