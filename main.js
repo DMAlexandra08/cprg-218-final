663d999b04c308281109a5f2a39004bd92a7f988
DMAlexandra08 <Alexandra.Armstrong@edu.sait.ca>
2025-04-18 11:30:04 -0600

    css
    started css. did header, and started working on titles.

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+const weather = document.getElementById('weather')
+
+
 fetch('https://api.openweathermap.org/data/2.5/weather?q=Cancun&appid=cfb3d1b143131b3e9f17add9eb31017a&units=metric')
 
 .then(response => response.json())
