fda603224918d535b62e68eaca9ac326ae8b1697
DMAlexandra08 <Alexandra.Armstrong@edu.sait.ca>
2025-04-18 15:57:49 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,3 @@
-const weather = document.getElementById('weather')
-const weathericon = document.getElementById('describe')
-const iconCode = data.weather[0].icon;
-const iconUrl = 'https://openweathermap.org/img/wn/${iconCode}@2x.png'
-weathericon.src = iconUrl;
-console.log(iconUrl);
-
-
 fetch('https://api.openweathermap.org/data/2.5/weather?q=Cancun&appid=cfb3d1b143131b3e9f17add9eb31017a&units=metric')
 
 .then(response => response.json())
@@ -14,3 +6,10 @@ fetch('https://api.openweathermap.org/data/2.5/weather?q=Cancun&appid=cfb3d1b143
     console.log(data.main.temp);
     weather.textContent = "Current Temperature : " + data.main.temp+" \u00B0C";
 });
+
+const weather = document.getElementById('weather')
+const weathericon = document.getElementById('describe')
+const iconCode = data.weather[0].icon;
+const iconUrl = 'https://openweathermap.org/img/wn/${iconCode}@2x.png';
+weathericon.src = iconUrl;
+console.log(iconUrl);
